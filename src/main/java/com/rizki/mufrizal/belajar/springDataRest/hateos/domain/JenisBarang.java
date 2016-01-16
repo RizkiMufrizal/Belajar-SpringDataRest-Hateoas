8cf3e9a917bcf1e001d64fb88abfd115ad272ac4
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-01-16 11:07:32 +0700

    tambah enum jenis barang

---
+++ src/main/java/com/rizki/mufrizal/belajar/springDataRest/hateos/domain/JenisBarang.java
@@ -0,0 +1,15 @@
+package com.rizki.mufrizal.belajar.springDataRest.hateos.domain;
+
+/**
+ *
+ * @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Since Jan 16, 2016
+ * @Time 9:17:37 AM
+ * @Encoding UTF-8
+ * @Project Belajar-SpringDataRest-Hateos
+ * @Package com.rizki.mufrizal.belajar.springDataRest.hateos.domain
+ *
+ */
+public enum JenisBarang {
+    gas, cair, padat
+}
