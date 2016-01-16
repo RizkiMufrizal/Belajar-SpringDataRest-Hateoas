cae21b0b49fa66a1c6bec53d4c1736f2b260dfb4
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-01-16 23:17:08 +0700

    tambah angular login controller

---
+++ src/main/resources/static/scripts/controllers/LoginController.js
@@ -0,0 +1,10 @@
+/**
+ *
+ * @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Since Jan 16, 2016
+ * @Time 9:07:03 PM
+ * @Encoding UTF-8
+ * @Project Belajar-SpringDataRest-Hateos
+ * @Package Expression package is undefined on line 8, column 15 in Templates/Other/javascript.js.
+ * 
+ */
