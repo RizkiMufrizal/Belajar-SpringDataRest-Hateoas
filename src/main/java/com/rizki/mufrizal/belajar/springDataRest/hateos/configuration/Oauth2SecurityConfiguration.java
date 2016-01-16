fbbb0dab2dd4c0a42596af8841915c77ace7b487
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-01-16 19:50:29 +0700

    Update konfigurasi oauth2

--- src/main/java/com/rizki/mufrizal/belajar/springDataRest/hateos/configuration/Oauth2SecurityConfiguration.java
+++ src/main/java/com/rizki/mufrizal/belajar/springDataRest/hateos/configuration/Oauth2SecurityConfiguration.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
@@ -46,8 +45,7 @@ public class Oauth2SecurityConfiguration {
         public void configure(HttpSecurity httpSecurity) throws Exception {
             httpSecurity
                     .authorizeRequests()
-                    .antMatchers("/api/**").authenticated()
-                    .antMatchers(HttpMethod.POST, "/api/user").permitAll()
+                    .antMatchers("/api").fullyAuthenticated()
                     .antMatchers("/home").permitAll()
                     .and()
                     .addFilterBefore(new CORSFilter(), ChannelProcessingFilter.class);
