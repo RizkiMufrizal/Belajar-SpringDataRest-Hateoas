6a37161ab60577052b73fb6de38635afcbd8efcc
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-01-16 19:51:21 +0700

    update authorization user

--- src/main/java/com/rizki/mufrizal/belajar/springDataRest/hateos/repository/UserRepository.java
+++ src/main/java/com/rizki/mufrizal/belajar/springDataRest/hateos/repository/UserRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.Description;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 /**
  *
@@ -17,9 +18,14 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
  * @Package com.rizki.mufrizal.belajar.springDataRest.hateos.repository
  *
  */
+@PreAuthorize("hasRole('ROLE_ADMIN')")
 @RepositoryRestResource(collectionResourceRel = "user", path = "user", collectionResourceDescription = @Description("API untuk crud data user"))
 public interface UserRepository extends PagingAndSortingRepository<User, String> {
 
     @Query("select u from User u left join fetch u.userRoles pd where u.email = :email")
     User LoginUser(@Param("email") String email);
+
+    @Override
+    @PreAuthorize("permitAll")
+    public <S extends User> S save(S s);
 }
