f331731ad0879f5b806aa8c1806465ac08c06079
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-01-17 17:59:59 +0700

    tambah user service

---
+++ src/main/resources/static/scripts/services/UserService.js
@@ -0,0 +1,30 @@
+/**
+ *
+ * @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Since Jan 17, 2016
+ * @Time 4:16:53 PM
+ * @Encoding UTF-8
+ * @Project Belajar-SpringDataRest-Hateos
+ * @Package Expression package is undefined on line 8, column 15 in Templates/Other/javascript.js.
+ *
+ */
+
+(function() {
+  'use strict';
+
+  angular.module('Belajar')
+    .factory('UserService', UserService);
+
+  UserService.$inject = ['$http'];
+
+  function UserService($http) {
+
+    return {
+      register: function(r) {
+        return $http.post('/api/user', r);
+      }
+    };
+
+  }
+
+})();
