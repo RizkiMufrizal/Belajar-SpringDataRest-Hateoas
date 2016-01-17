931e8c8a4881992a6804195df32d049b581f257b
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-01-17 13:24:09 +0700

    update login controller

--- src/main/resources/static/scripts/controllers/LoginController.js
+++ src/main/resources/static/scripts/controllers/LoginController.js
@@ -6,5 +6,32 @@
  * @Encoding UTF-8
  * @Project Belajar-SpringDataRest-Hateos
  * @Package Expression package is undefined on line 8, column 15 in Templates/Other/javascript.js.
- * 
+ *
  */
+
+(function() {
+  'use strict';
+  angular.module('Belajar')
+    .controller('LoginController', LoginController);
+
+  LoginController.$inject = ['$cookies', 'LoginService', '$state'];
+
+  function LoginController($cookies, LoginService, $state) {
+    var login = this;
+
+    login.inputLogin = {};
+
+    login.url = '';
+
+    login.processLogin = function(l) {
+      LoginService.login(l).success(function successCallback(response) {
+        $cookies.put('token', response.access_token);
+        $state.go('home');
+      }, function errorCallback(response) {
+        console.log(response);
+      });
+    };
+
+  }
+
+})();
