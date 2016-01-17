d011359445636d1360b0488dfa4d937c87e00b47
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-01-17 13:23:15 +0700

    tambah directive ngLogin

---
+++ src/main/resources/static/scripts/directives/ngLogin.js
@@ -0,0 +1,25 @@
+/**
+ *
+ * @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Since Jan 17, 2016
+ * @Time 9:10:21 AM
+ * @Encoding UTF-8
+ * @Project Belajar-SpringDataRest-Hateos
+ * @Package Expression package is undefined on line 8, column 15 in Templates/Other/javascript.js.
+ *
+ */
+
+(function() {
+  'use strict';
+  angular.module('Belajar')
+    .directive('ngLogin', ngLogin);
+
+  function ngLogin() {
+    return {
+      restrict: 'E',
+      templateUrl: '../../templates/login.template.html',
+      controller: 'LoginController',
+      controllerAs: 'login'
+    };
+  }
+})();
