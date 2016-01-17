98eea98079ab52c4677cb2ce14676882919cf663
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-01-17 23:10:58 +0700

    tambah logout controller angular

---
+++ src/main/resources/static/scripts/controllers/LogoutController.js
@@ -0,0 +1,32 @@
+/**
+ *
+ * @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Since Jan 17, 2016
+ * @Time 9:59:58 PM
+ * @Encoding UTF-8
+ * @Project Belajar-SpringDataRest-Hateos
+ * @Package Expression package is undefined on line 8, column 15 in Templates/Other/javascript.js.
+ *
+ */
+
+(function() {
+  'use strict';
+  angular.module('Belajar')
+    .controller('LogoutController', LogoutController);
+
+  LogoutController.$inject = ['$cookies', '$window'];
+
+  function LogoutController($cookies, $window) {
+
+    function logout() {
+      $cookies.remove('token');
+      $cookies.remove('refreshToken');
+      $window.location.href = '/#/';
+      console.log('cek');
+    }
+
+    logout();
+
+  }
+
+})();
