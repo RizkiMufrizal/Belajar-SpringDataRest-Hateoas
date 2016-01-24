9214170c653ba3d0c7ea78f03c787b2bedc0ae91
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-01-24 10:28:59 +0700

    fix error untuk deploy tomcat

--- src/main/java/com/rizki/mufrizal/belajar/springDataRest/hateos/repository/eventHandler/UserRepositoryEventHandler.java
+++ src/main/java/com/rizki/mufrizal/belajar/springDataRest/hateos/repository/eventHandler/UserRepositoryEventHandler.java
@@ -2,13 +2,13 @@ package com.rizki.mufrizal.belajar.springDataRest.hateos.repository.eventHandler
 
 import com.rizki.mufrizal.belajar.springDataRest.hateos.domain.User;
 import com.rizki.mufrizal.belajar.springDataRest.hateos.domain.UserRole;
-import java.util.HashSet;
-import java.util.Set;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.core.annotation.HandleBeforeCreate;
 import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  *
@@ -22,14 +22,12 @@ import org.springframework.stereotype.Component;
  *
  */
 @Component
-@RepositoryEventHandler(User.class)
+@RepositoryEventHandler(value = {User.class})
 public class UserRepositoryEventHandler {
-
-    @Autowired
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @HandleBeforeCreate
     public void handleUserCreate(User user) {
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
         user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
 
         UserRole userRole = new UserRole();
